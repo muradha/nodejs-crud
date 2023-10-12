a06402d7175d749c1b9139b96592b59dfd22a75c
muradha <mhdrizki098@gmail.com>
2023-10-12 09:02:03 +0700

    `add` : body-parser and method override

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require("express");
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 const mahasiswaRouter = require('./routes/mahasiswa');
 const { errorMiddleware } = require("./middleware/errorMiddleware");
 
 let app = express();
 const port = 8080;
 
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        // look in urlencoded POST bodies and delete it
+        var method = req.body._method
+        delete req.body._method
+        return method
+    }
+}));
 app.use(express.json());
 
 app.use(mahasiswaRouter);
